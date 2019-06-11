b0b45515f1167315d31d8aca063d7ad8f93fcce3
xMambo <millerdesigncotn@gmail.com>
2019-06-11 15:02:24 -0500

    Updates

---
+++ precache-manifest.15ed28aa4b914229ea4c725190f0aa0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa5ee0f74dccf67770d93f5be33fef1",
+    "url": "/googlebooks/index.html"
+  },
+  {
+    "revision": "f3bd6e98334fb7e26c5c",
+    "url": "/googlebooks/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/googlebooks/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f3bd6e98334fb7e26c5c",
+    "url": "/googlebooks/static/js/main.473f053d.chunk.js"
+  },
+  {
+    "revision": "97c07a0dbdb079a0dd9d",
+    "url": "/googlebooks/static/js/runtime~main.fb3a3354.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/googlebooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
